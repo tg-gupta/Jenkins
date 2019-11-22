cd0aa3b937b1b814e424078f94aa96d887016d83
tarun.gupta <tarun.gupta@tarunguptas-mbp.clients.asia-pac.shell.com>
2019-11-22 10:55:37 +0530

    created main file

--- main.tf
+++ main.tf
@@ -1,15 +1,16 @@
 provider "aws" {
-    region = "ap-south-1"
-    profile = "ShellPowerUser"	
+  region  = "ap-south-1"
+  profile = "ShellPowerUser"
 }
-variable "ami" {
+variable "pt_ami" {
   default = "ami-0ca4ee4403ecb3a4e"
 }
 
 resource "aws_instance" "jenkins_master" {
-  ami           = "${var.ami}"
-  instance_type = "t2.micro"
+  ami                    = var.pt_ami
+  instance_type          = "t2.micro"
   vpc_security_group_ids = ["${aws_security_group.sg_jenkins.id}"]
+  # aws_security_group = "${aws_security_group.sg_jenkins.name}"
 
   tags = {
     Name = "Jenkins Master"
@@ -17,65 +18,65 @@ resource "aws_instance" "jenkins_master" {
 }
 
 
-  resource "aws_security_group" "sg_jenkins" {
-  name = "sg_${timestamp}"
+resource "aws_security_group" "sg_jenkins" {
+  name        = "sg1"
   description = "Allows all traffic"
 
   # SSH
   ingress {
     from_port = 22
-    to_port = 22
-    protocol = "tcp"
+    to_port   = 22
+    protocol  = "tcp"
     cidr_blocks = [
-      "0.0.0.0/0"]
+    "0.0.0.0/0"]
   }
 
   # HTTP
   ingress {
     from_port = 80
-    to_port = 80
-    protocol = "tcp"
+    to_port   = 80
+    protocol  = "tcp"
     cidr_blocks = [
-      "0.0.0.0/0"]
+    "0.0.0.0/0"]
   }
 
   # HTTPS
   ingress {
     from_port = 443
-    to_port = 443
-    protocol = "tcp"
+    to_port   = 443
+    protocol  = "tcp"
     cidr_blocks = [
-      "0.0.0.0/0"]
+    "0.0.0.0/0"]
   }
 
   # Jenkins JNLP port
   ingress {
     from_port = 50000
-    to_port = 50000
-    protocol = "tcp"
+    to_port   = 50000
+    protocol  = "tcp"
     cidr_blocks = [
-      "0.0.0.0/0"]
+    "0.0.0.0/0"]
   }
 
   # Weave Scope port
   ingress {
     from_port = 4040
-    to_port = 4040
-    protocol = "tcp"
+    to_port   = 4040
+    protocol  = "tcp"
     cidr_blocks = [
-      "0.0.0.0/0"]
+    "0.0.0.0/0"]
   }
 
   egress {
     from_port = 0
-    to_port = 0
-    protocol = "-1"
+    to_port   = 0
+    protocol  = "-1"
     cidr_blocks = [
-      "0.0.0.0/0"]
+    "0.0.0.0/0"]
   }
 }
 
 
- output "instance_id" {
-    value = "${aws_instance.jenkins_master.id}"
+output "instance_id" {
+  value = "${aws_instance.jenkins_master.id}"
 }
