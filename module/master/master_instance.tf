6f8bc39552618175262bb239f0cee003a89aa7a5
tarun.gupta <tarun.gupta@tarunguptas-mbp.clients.asia-pac.shell.com>
2019-11-22 13:58:07 +0530

    changes

--- module/master/master_instance.tf
+++ module/master/master_instance.tf
@@ -4,18 +4,12 @@ resource "aws_instance" "pt_master_module" {
   instance_type          = "t2.micro"
   vpc_security_group_ids = ["${aws_security_group.sg_jenkins.id}"]
   # aws_security_group = "${aws_security_group.sg_jenkins.name}"
-  key_name = "master_key"
+  key_name = "master"
   tags = {
     Name = "Jenkins Master_PT"
   }
 }
 
-
-
-resource "aws_key_pair" "deployer" {
-  key_name   = "admin-key"
-  public_key = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQDbLF4lLWJsQhSJsjqeR+jfoRKKbMKpSx1jI+qQ3VBoQ2TgXFn98tIvvThQHtukGDZse30EyECpqW4K00a3vhSRI3uJuk6fxmswN7MDoCAAAjUEWc42SZQzw77VNpIlGkzrQPS3GjAJqho7ti1Wsz4hKhEyCIqTnQGGcNdv0gOOuyO0n0iNCFCQDC7bokFJnswygbNRMvQxeEuHJJETulHUAjAL9UG831ynrzzO+lWee8aSYNaJ1DJxVUHs+krhR2J7+nEkHjaiaKSjYUc8+FHLCa8pGMIpvYFk8ZOkTbVbPJODljl8jJR9RYZEj8eUihyLRggQ5FyKJslsEBX+0XLd tarun.gupta@tarunguptas-mbp.clients.asia-pac.shell.com"
-}
 resource "aws_security_group" "sg_jenkins" {
   name        = "sg2"
   description = "Allows all traffic"
